49f329610cb7483e0cb4cee7480689f60add902a
hwse <hagenweisse@gmail.com>
2020-01-07 18:22:27 +0100

    init application

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use gtk::{Application, ApplicationWindow, Button, GtkWindowExt, ButtonExt, ContainerExt, WidgetExt};
+use gio::ApplicationExt;
+use gio::prelude::ApplicationExtManual;
+
+fn main() {
+    let application = Application::new(
+        Some("com.github.gtk-rs.examples.basic"),
+        Default::default(),
+    ).expect("failed to initialize GTK application");
+
+    application.connect_activate(|app| {
+        let window = ApplicationWindow::new(app);
+        window.set_title("First GTK+ Program");
+        window.set_default_size(350, 70);
+
+        let button = Button::new_with_label("Click me!");
+        button.connect_clicked(|_| {
+            println!("Clicked!");
+        });
+        window.add(&button);
+
+        window.show_all();
+    });
+
+    application.run(&[]);
+}
